09bd8a27822dc509678c2cf1cc48c163fd51eba0
pertusatti22 <pertusatti22@gmail.com>
2022-11-07 23:18:49 -0300

    Flutter ValueNotifier #5 - Builders Parte 2

--- lib/src/home/home_page.dart
+++ lib/src/home/home_page.dart
@@ -30,11 +30,11 @@ class _MyHomePageState extends State<MyHomePage> {
             const Text(
               'You have pushed the button this many times:',
             ),
-            AnimatedBuilder(
-                animation: counter,
-                builder: (context, child) {
+            ValueListenableBuilder<int>(
+                valueListenable: counter,
+                builder: (context, value, child) {
                   return Text(
-                    '${counter.value}',
+                    '$value',
                     style: Theme.of(context).textTheme.headline4,
                   );
                 })
