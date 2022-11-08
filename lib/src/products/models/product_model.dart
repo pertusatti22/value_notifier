f67b285580f054428a33bb5dd31c625f90f39161
pertusatti22 <pertusatti22@gmail.com>
2022-11-07 23:38:18 -0300

    Flutter ValueNotifier #6 - Iniciando servidor e criando Service

---
+++ lib/src/products/models/product_model.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+
+class ProductModel {
+  final String id;
+  final String title;
+
+  const ProductModel({
+    required this.id,
+    required this.title,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+    };
+  }
+
+  factory ProductModel.fromMap(Map<String, dynamic> map) {
+    return ProductModel(
+      id: map['id'] ?? '',
+      title: map['title'] ?? '',
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory ProductModel.fromJson(String source) =>
+      const ProductModel(id: 'id', title: 'title');
+}
---
+++ lib/src/products/services/products_service.dart
@@ -0,0 +1,15 @@
+import 'package:uno/uno.dart';
+import 'package:value_notifier/src/products/models/product_model.dart';
+
+class ProductsService {
+  final Uno uno;
+
+  ProductsService(this.uno);
+
+  Future<List<ProductModel>> fetchProducts() async {
+    final response = await uno.get('http"://localhost:3031/products');
+    final list = response.data as List;
+    final products = list.map((e) => ProductModel.fromMap(e)).toList();
+    return products;
+  }
+}
